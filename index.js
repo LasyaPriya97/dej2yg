4e82aa3371423cbb0995fae3d537b5037865e9ab
LasyaPriya97 <pulicherla.lasya@gmail.com>
2019-10-16 16:28:46 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import First from './first';
+import Third from './third';
 
 
-ReactDOM.render(<First/>, document.querySelector('#root'));
+ReactDOM.render(<Third/>, document.querySelector('#root'));
     
