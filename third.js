e10c06d869ad467fecc514ecf5c985c0a5883f20
LasyaPriya97 <pulicherla.lasya@gmail.com>
2019-10-16 18:29:56 +0530

    1 file modified

--- third.js
+++ third.js
@@ -10,18 +10,74 @@ import Paper from '@material-ui/core/Paper';
 import Popper from '@material-ui/core/Popper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
+import { withStyles } from '@material-ui/core/styles';
+import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
+import MuiExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import MuiExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
+const ExpansionPanel = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      margin: 'auto',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanel);
+
+const ExpansionPanelSummary = withStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    borderBottom: '1px solid rgba(0, 0, 0, .125)',
+    marginBottom: -1,
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56,
+    },
+  },
+  content: {
+    '&$expanded': {
+      margin: '12px 0',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanelSummary);
+
+const ExpansionPanelDetails = withStyles(theme => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiExpansionPanelDetails);
 
 const options = ['Product Type 1', 'Product Type 2', 'Product Type 3'];
 const useStyles = makeStyles(theme=>({
+  root:{
+    flexGrow:1,
+  },
   button:{
     margin: theme.spacing(1),
+    align:"center",
   },
   input:{
      display:'none',
   },
 }));
 export default function SplitButton() {
+  const [expanded, setExpanded] = React.useState('panel1');
+
+  const handleChange = panel => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
    const classes=useStyles();
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
@@ -53,8 +109,9 @@ export default function SplitButton() {
   };
 
   return (
-    <Grid container direction="column" alignItems="center">
-      <Grid item xs={12}>
+    <div className={classes.root}>
+    <Grid container>
+      <Grid item xs={6} align="center">
         <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
           <Button onClick={handleClick}>{options[selectedIndex]}</Button>
           <Button
@@ -93,10 +150,30 @@ export default function SplitButton() {
             </Grow>
           )}
         </Popper>
-        </Grid>
-        <br/>
-        <Button variant="contained" color="initial" className={classes.button}> Download</Button><br/>
+       </Grid>
+       <Grid xs={6} align="center">
+       <ExpansionPanel square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+        <ExpansionPanelSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>Template List</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            selected product type 1
+            <Divider/>
+            selected product type 2
+            <Divider/>
+            selected product type 3
+        </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      </Grid>
+        <br/><br/><br/>
+        <Grid xs={6} align="center">
+        <Button variant="contained" color="initial" className={classes.button}> Download</Button></Grid>
+        <Grid xs={6} align="center">
         <Button variant="contained" color="secondary" className={classes.button}>Upload Files</Button>
         </Grid>
+        </Grid>
+        </div>
   );
 }
