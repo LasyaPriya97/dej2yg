1a160c908cdca37da6d82ea17beb45d3d446c3b0
LasyaPriya97 <pulicherla.lasya@gmail.com>
2019-10-16 11:30:33 +0530

    1 file modified

--- demo.js
+++ demo.js
@@ -4,7 +4,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import Box from '@material-ui/core/Box';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -30,14 +31,21 @@ export default function SimpleContainer() {
     <Button color="inherit" className={classes.Toolbarbutton}>Signout</Button>
     </Toolbar>
     </AppBar>
-       <Typography variant="h3">
-        Hello,
+       <Typography variant="h3"> Hello,
         </Typography> 
+        <Box p={2} bgcolor="background.paper">
+        </Box>
         <Typography variant="h4" align='center'> Arshiya Farzeen Avaran
         </Typography>
+        <Box p={2} bgcolor="background.paper">
+        </Box>
+        <Grid container>
+        <Grid item xs={12} align="center"> 
         <Button variant="contained"  color="default" className={classes.button}>Download Item Templates</Button>
         <Button variant="contained"  color="default" className={classes.button}>upload</Button> 
         <Button variant="contained"  color="default" className={classes.button}>Status of Batches</Button>
+        </Grid>
+        </Grid>
         </div>
     );
 }
