027d222da540768ca2d4f61a647e8380ddbfcd4c
LasyaPriya97 <pulicherla.lasya@gmail.com>
2019-10-18 18:29:15 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import First from './First';
+import Second from './Second';
 import Four from './Four';
-ReactDOM.render(<Fou/>,document.querySelector('#root'));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Route
+          path="/"
+          render={({ location }) => (
+            <Fragment>
+        
+              <Tabs value={location.pathname}>
+                <Tab label="Item One" component={Link} to="/" />
+                <Tab label="Item Two" component={Link} to="/second" />
+                <Tab
+                  label="Item Three"
+                   component={Link}
+                  to="/four"
+                />
+              </Tabs>
+              <Switch>
+                <Route path="/second" render={() => <div><Second/></div>} />
+                <Route path="/four" render={() => <div><Four/></div>} />
+                <Route path="/" render={() => <div><First/></div>} />
+              </Switch>
+            
+            </Fragment>
+          )}
+        />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
