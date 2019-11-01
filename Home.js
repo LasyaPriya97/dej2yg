4caf834c64d1a3e98733ed574a50bf83e17695cd
LasyaPriya97 <pulicherla.lasya@gmail.com>
2019-11-01 16:56:31 +0530

    3 files modified, 2 added

---
+++ Home.js
@@ -0,0 +1,79 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
+import One from "./One";
+import Two from "./Two";
+import Three from "./Three";
+import Four from "./Four";
+import Five from "./Five";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Route
+          path="/"
+          render={({ location }) => (
+            <React.Fragment>
+              <AppBar position="static">
+                <Tabs value={location.pathname}>
+                  <Tab label="Page One" component={Link} to="/" />
+                  <Tab label="Page Two" component={Link} to="/two" />
+                  <Tab label="Page Three" component={Link} to="/three" />
+                  <Tab label="Page Four" component={Link} to="/four"/>
+                  <Tab label="Page Five" component={Link} to="/five" />
+                </Tabs>
+              </AppBar>
+              <br />
+              <Switch>
+                <Route
+                  path="/Two"
+                  render={() => (
+                    <div>
+                      <Two />
+                    </div>
+                  )}
+                />
+                <Route
+                  path="/Three"
+                  render={() => (
+                    <div>
+                      <Three />
+                    </div>
+                  )}
+                />
+                <Route
+                  path="/Four"
+                  render={() => (
+                    <div>
+                      <Four />
+                    </div>
+                  )}
+                />
+                <Route
+                  path="/Five"
+                  render={() => (
+                    <div>
+                      <Five />
+                    </div>
+                  )}
+                />
+                <Route
+                  path="/"
+                  render={() => (
+                    <div>
+                      <One />
+                    </div>
+                  )}
+                />
+              </Switch>
+            </React.Fragment>
+          )}
+        />
+      </div>
+    </BrowserRouter>
+  );
+}
--- Six.js
+++ Six.js
@@ -4,7 +4,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import RaisedButton from "@material-ui/Button";
 import Link from "@material-ui/core/Link";
@@ -26,33 +25,34 @@ const useStyles = makeStyles(theme => ({
       }
     }));
 
-   export default function simpleContainer() {
-    const classes = useStyles();
-    return (
+export default function simpleContainer(){
+  const classes = useStyles();
+  const isButtonClicked =true;
+  const value= isButtonClicked == true?
+  return (
       <div className={classes.root}>
         <AppBar position="static">
           <Toolbar>
             <Typography variant="body1"> Hello,</Typography>
             <Typography
               variant="body1"
-              align="center"
-              className={classes.Toolbarbutton}
-            >
+               className={classes.Toolbarbutton}>
               Arshiya Farzeen Avaran
             </Typography>
-            <Button color="inherit" className={classes.Toolbarbutton} >
-              SignOut
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <br />
-
-        <Grid container>
+            <Button color="inherit" className={classes.Toolbarbutton} onClick = {isButtonClicked ? (
+              <div>
+       <Typography variant="body1">Please click <span>
+       <Link component="button" variant="body2">here</Link></span>to login
+       </Typography>
+       </div>
+            ):(
+            <div>
+     <Grid container>
           <Grid item xs={12} align="center">
             <Button
               variant="contained"
               color="default"
-              className={classes.button}
+             className={classes.button}
             >
               Download Item Templates
             </Button>
@@ -66,14 +66,20 @@ const useStyles = makeStyles(theme => ({
             <Button
               variant="contained"
               color="default"
-              className={classes.button}
+             className={classes.button} 
             >
               Status of Batches
             </Button>
           </Grid>
         </Grid>
+        </div>  
+            )} >
+              SignOut
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <br />
+        
       </div>
     );
-  }
-
-
+}
